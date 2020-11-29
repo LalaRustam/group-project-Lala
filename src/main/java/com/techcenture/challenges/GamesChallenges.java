500cbfe5b175a65146cb092567d5456a722dac4a
TairovichJR <yokubov.omonjon@gmail.com>
2020-11-29 10:21:08 -0500

    experiment branch

---
+++ src/main/java/com/techcenture/challenges/GamesChallenges.java
@@ -0,0 +1,23 @@
+package com.techcenture.challenges;
+
+import com.sun.tools.jdeprscan.scan.Scan;
+
+import java.util.Scanner;
+
+public class GamesChallenges {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter any key to start the game");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");  System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+
+        System.out.println("The end");
+
+    }
+}
